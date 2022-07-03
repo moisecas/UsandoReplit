b3622ea3bf4b7414b7eef39f7bcdfd735a286a55
Moisés <62753698+moisecas@users.noreply.github.com>
2022-07-03 22:32:07 +0000

    creando otra clase

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import {user} from './user.js'
+import {product} from './product.js'
 
 const name = new user('moises','castro')
-
+console.log("user.js") 
 console.log(name) 
-console.log("manejo en replit") 
+
+console.log("product.js") 
+
+
+console.log(new product('skins', 29.000)) 
---
+++ product.js
@@ -0,0 +1,7 @@
+export class product{
+  constructor(name,price){
+    this.name=name, //this hace parte de 
+    this.price=price
+    
+  }
+}
